0d9d7bd46516b9a24e5909f4a19d97cb8d928a0e
杨思雨 <41177678+yangsiyu0@users.noreply.github.com>
2018-12-03 14:51:12 +0800

    Update mvvm1.js

--- mvvm1.js
+++ mvvm1.js
@@ -3,7 +3,7 @@ class MVVM{
         this.el = option.el;
         this.data = option.data;
         // 定义响应式
-        new Observer(this.data);
+        new Observer(this,this.data);
         // 编译
         new Compile(this.el,this.data);
     }
